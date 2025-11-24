7dcc6c63a65856d2293724243983ca3d15331d60
KimSnowFlower <kimbona1148@gmail.com>
2025-11-24 14:49:55 +0900

    fix: #24 allow external access by binding server to 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const config = require("./config/env");
 const app = require("./app");
 
-app.listen(config.port, () => {
+const HOST = "0.0.0.0"
+app.listen(config.port, HOST, () => {
     console.log(`TestWeaver API running on port ${config.port}`);
 });
